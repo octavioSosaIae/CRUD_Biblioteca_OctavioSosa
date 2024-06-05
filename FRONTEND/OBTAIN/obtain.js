ee119e716d5f38c5d6030e1bce1072fd25759601
ottisosa <ottisosa10@gmail.com>
2024-06-05 20:59:25 -0300

    agrego el selector para ordenar por fecha, precio y nombre

--- FRONTEND/OBTAIN/obtain.js
+++ FRONTEND/OBTAIN/obtain.js
@@ -6,25 +6,25 @@ window.onload = () => {
 
 async function obtainBooks() {
 
-    let url = 'http://localhost/crud_biblioteca_octaviososa/BACKEND/CONTROLLER/bookController.php?function=obtain';
+    let url = 'http://localhost/crud_biblioteca_octaviososa/BACKEND/CONTROLLER/bookController.php?function=orderDate';
     let query = await fetch(url);
     let data = await query.json();
     booksList = data;
     showBooks(data);
 }
 
-function showBooks(book) {
+function showBooks(books) {
 
     let tbodyElement = document.querySelector("#tableBooks");
     tbodyElement.innerHTML = "";
-    for (let i = 0; i < book.length; i++) {
+    for (let i = 0; i < books.length; i++) {
 
         tbodyElement.innerHTML += `               
                 <tr>
-                <td>${book[i].id}</td>
-                <td>${book[i].nombre}</td>
-                <td>${book[i].fecha}</td>
-                <td>${book[i].precio}</td>    
+                <td>${books[i].id}</td>
+                <td>${books[i].nombre}</td>
+                <td>${books[i].fecha}</td>
+                <td>${books[i].precio}</td>    
                 </tr>
                 `;
 
@@ -34,17 +34,36 @@ function showBooks(book) {
 
 function addEventSelect() {
 
-    let filter = document.querySelector("#select")
+    let filter = document.querySelector("#select")   //  query selector del selector para ordenar las tablas por nombre, fecha y precio  //
 
-    filter.onchange = () => {
+    filter.onchange = async () => {      
 
-        let value = filter.options[filter.selectedIndex].value;
-
-    
+        let value = filter.options[filter.selectedIndex].value;  
 
 
+        let url;
+
+        switch (value) {             //  switch que segun la variable value cambia la ruta que lleva a la funcion con la consulta SQL  //
+
+            case "name":
+                url = 'http://localhost/crud_biblioteca_octaviososa/BACKEND/CONTROLLER/bookController.php?function=orderName';
+                break;
+            case "date":
+                url = 'http://localhost/crud_biblioteca_octaviososa/BACKEND/CONTROLLER/bookController.php?function=orderDate';
+                break;
+            case "price":
+                url = 'http://localhost/crud_biblioteca_octaviososa/BACKEND/CONTROLLER/bookController.php?function=orderPrice';
+                break;
+            default:
+                url = 'http://localhost/crud_biblioteca_octaviososa/BACKEND/CONTROLLER/bookController.php?function=orderDate';
+        }
+        try {
+            let query = await fetch(url);
+            let data = await query.json();     
+            showBooks(data);
+        } catch (error) {
+            console.error('Error al mostrar los libros :(', error);     
+        }
 
     }
-
-
 }
